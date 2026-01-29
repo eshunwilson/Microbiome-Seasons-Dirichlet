d34b4d22c77e5c1181cc82d64754bb26b57f6dd6
Franceen Eshun-Wilson <60021619+eshunwilson@users.noreply.github.com>
2026-01-29 14:26:01 +0100

    Clean up header comments in Microbiome_Seasonal_Dirichlet.py
    Removed unnecessary shebang, encoding declaration, and docstring.

--- Microbiome_Seasonal_Dirichlet.py
+++ Microbiome_Seasonal_Dirichlet.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb 12 16:43:35 2025
-
-@author: mtn309
-"""
+# Simulation of sesonal changes of microbiome using Dirichlet sampling
 
 import numpy as np
 import matplotlib.pyplot as plt
